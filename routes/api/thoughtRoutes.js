9f0a3ab06e91007158d53b7993280008a96292e0
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-10-23 19:52:49 -0700

    api routes are complete for now

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,20 +1,20 @@
 const router = require('express').Router();
 const {
-  getCourses,
-  getSingleCourse,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-} = require('../../controllers/courseController.js');
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require('../../controllers/thoughtController.js');
 
-// /api/courses
-router.route('/').get(getCourses).post(createCourse);
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
 
-// /api/courses/:courseId
-router
-  .route('/:courseId')
-  .get(getSingleCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+// /api/courses/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createThought);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeThought);
 
 module.exports = router;
