f9a942d984718e334ecd395244c4d1e2095cf032
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-10-23 19:42:36 -0700

    updated userRoute with friends not thoughts

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,21 +3,22 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
   deleteUser,
-  addThought,
-  removeThoughtt,
+  addFriend,
+  removeFriend,
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// /api/users/:userId/thoughts
-router.route('/:userId/thoughts').post(addThought);
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
 
-// /api/users/:userId/thoughts/:thoughtId
-router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
+// /api/users/:userId/tfriends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
 
 module.exports = router;
